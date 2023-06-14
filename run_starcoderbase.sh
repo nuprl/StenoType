d3daefa78cec4c68d5e13172cbb135b2c6b5cab3
Ming-Ho Yee <mh@mhyee.com>
2023-06-14 17:36:42 +0000

    specify devices for run_starcoderbase.sh

--- run_starcoderbase.sh
+++ run_starcoderbase.sh
@@ -4,10 +4,14 @@
 # override by calling script with `DOCKER=docker ./run_starcoderbase.sh`
 : ${DOCKER:=podman}
 
+# use GPU 0 by default
+# override by calling script with `DEVICES=2 ./run_starcoderbase.sh`
+: ${DEVICES:=0}
+
 $DOCKER run \
     -p 8787:80 \
     -v $(pwd)/../models:/data \
-    -e NVIDIA_VISIBLE_DEVICES=1 \
+    -e NVIDIA_VISIBLE_DEVICES=$DEVICES \
     -e HF_HUB_ENABLE_HF_TRANSER=0 \
     ghcr.io/huggingface/text-generation-inference:0.8 \
     --model-id /data/starcoderbase \
