2895da6218f2f58a16b16b2daf722b74912bda7f
Ming-Ho Yee <mh@mhyee.com>
2023-06-15 21:19:05 +0000

    Setup baseline experiment. Still need to evaluate accuracy

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from datasets import Dataset # type: ignore
+from datasets import (Dataset,              # type: ignore
+                      DatasetDict,
+                      IterableDataset,
+                      IterableDatasetDict)
 from pathlib import Path
+from typing import Any
 import argparse
 import datasets
+import pandas as pd
 import os
 
 from model import Model
 from type_inference import TypeInference
+import util
+
+COLUMN_WITHOUT_TYPES = "content_without_types"
 
 def parse_args():
     cpu_count = os.cpu_count()
@@ -22,20 +30,37 @@ def parse_args():
     parser.add_argument(
         "--revision",
         type=str,
-        help="Dataset revision, if loading from the Hub")
+        default="main",
+        help="Dataset revision, if loading from the Hub; defaults to 'main'")
     parser.add_argument(
         "--split",
         type=str,
-        help="Dataset split, if loading from the Hub")
+        default="train",
+        help="Dataset split, if loading from the Hub; defaults to 'train'")
+    parser.add_argument(
+        "--content-column",
+        type=str,
+        default="content",
+        help="Column with the file contents; defaults to 'content'")
     parser.add_argument(
         "--workers",
         type=int,
         default=cpu_count,
-        help=f"maximum number of workers to use, defaults to {cpu_count}")
+        help=f"maximum number of workers to use; defaults to {cpu_count}")
 
     return parser.parse_args()
 
-def load_dataset(dataset, split="train", revision="main", workers=1):
+def load_dataset(
+    dataset: str,
+    split: str,
+    revision: str,
+    workers: int
+) -> Dataset | DatasetDict | IterableDataset | IterableDatasetDict:
+    """
+    Load a dataset. Tries to interpret dataset as a path and loads a local file
+    (in Parquet or JSON Lines format) or directory. If the path does not exist,
+    load the dataset from the Hugging Face Hub.
+    """
     if Path(dataset).exists():
         print(f"Loading dataset from {dataset} from disk...", flush=True)
         if dataset.endswith(".parquet"):
@@ -51,68 +76,65 @@ def load_dataset(dataset, split="train", revision="main", workers=1):
                                      revision=revision,
                                      num_proc=workers)
 
+def add_column_without_types(example: dict[str, Any], column: str) -> dict[str, Any]:
+    # Delete type annotations and definitions
+    content = example[column]
+    stripped = util.delete_types(content)
+    example[COLUMN_WITHOUT_TYPES] = stripped
+    return example
+
+def run_baseline(
+    example: dict[str, Any],
+    typeinf: TypeInference,
+    column: str
+) -> dict[str, Any]:
+    # TODO: For now, assume TypeScript with type annotations and definitions
+    # that we strip. Later we can preprocess the dataset or look at JavaScript
+    # datasets.
+    content = example[column]
+    stripped = example[COLUMN_WITHOUT_TYPES]
+
+    # Run type inference
+    output = typeinf.infer_with_definitions(stripped)
+
+    # TODO: Evaluate with accuracy. Save to "accuracy" column
+    # Later we can type check (using TypeScript LSP or compiler)
+
+    return {
+        "hexsha": example["hexsha"],
+        "max_stars_repo_path": example["max_stars_repo_path"],
+        "max_stars_repo_name": example["max_stars_repo_name"],
+        "content": content,
+        "output": output,
+        "accuracy": 0.0
+    }
+
 def main():
     args = parse_args()
 
-    model = Model()
-    typeinf = TypeInference(model)
-
-    # For now, use TS with no type annotations
-    # Still need to strip type definitions
-    # TODO: add a column with type definitions removed, or remove them on-the-fly?
-    # For now, use content column (with type defs and anns) and remove on the fly
-    # Later we can look try a JS dataset
+    typeinf = TypeInference(Model())
     dataset = load_dataset(args.dataset, args.split, args.revision, args.workers)
 
-    example = dataset[6]["content_without_annotations"]
-    # example1 = """
-    # function aaa(x,
-    #              y)
-    #              { return x + y; }
-    # function abc(x, y?) { return x; }
-    # function def(x = 0) { return x; }
-    # function ghi({x, y}) { return x + y; }
-    # let jkl = function(x, y?) { return x; }
-    # let mno = function(x = 0) { return x; }
-    # let pqr = function({x, y}) { return x + y; }
-    # const a = x1 => x1;
-    # const b = (x2) => x2;
-    # const c = x3 => x3;
-    # const d = (x4, y4) => x4 + y4;
-    # var {e, f} = abcdef;
-    # class A {
-    #     x;
-    #     y = 42;
-    # }
-    # """
-    # example2 = """
-    # function fib(n) {
-    #   if (n < 1) {
-    #     return 0;
-    #   } else {
-    #     return fib(n-1) + fib(n-2)
-    #   }
-    # }
-    # """
-    # example3 = """
-    # function distance(p1, p2) {
-    #     const dx = p2.x - p1.x;
-    #     const dy = p2.y - p1.y;
-    #     return Math.sqrt(dx*dx + dy*dy);
-    # }
-    # """
-    # result = typeinf.infer(example1)
-    result = typeinf.infer_with_definitions(example)
-    print(result)
+    # Add column without types, then filter to remove empty rows (since removing
+    # types may end up removing everything)
+    num_examples = len(dataset)
+    dataset = dataset.map(lambda e: add_column_without_types(e, args.content_column),
+                          num_proc=args.workers)
+    dataset = dataset.filter(lambda e: not util.is_empty(e[COLUMN_WITHOUT_TYPES]),
+                             num_proc=args.workers)
+    num_removed = num_examples - len(dataset)
 
-    # TODO
-    # - Evaluation
-    #   - Iterate over dataset
-    #   - strip type annotations and definitions
-    #   - run inference
-    #   - evaluate with accuracy
-    #     - Later we can type check (using TypeScript LSP?)
-    # - Then output results
+    # Run the baseline experiment
+    result = dataset.map(lambda e: run_baseline(e, typeinf, COLUMN_WITHOUT_TYPES),
+                         num_proc=args.workers)
+
+    # Print results statistics
+    print("Number of examples in the original:", num_examples)
+    print("Number of examples skipped:", num_removed)
+    accuracy = pd.DataFrame({"accuracy": result["accuracy"]})
+    print(accuracy.describe())
+
+    # TODO: save result dataset to disk
 
 if __name__ == "__main__":
     main()
