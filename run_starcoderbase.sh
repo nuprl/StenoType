735ccae51e2a6528ccb8c22705ad5f791caf80b0
Ming-Ho Yee <mh@mhyee.com>
2023-07-24 20:43:56 +0000

    run_starcoderbase script supports other model names

--- run_starcoderbase.sh
+++ run_starcoderbase.sh
@@ -4,6 +4,10 @@
 # variable does not exist, look in ../models
 : ${MODELS_DIRECTORY:=$(pwd)/../models}
 
+# $MODELS_DIRECTORY is mounted to /data, and the default model is starcoderbase
+# override by calling the script with `MODEL_NAME=starcoder ./run_starcoderbase.sh`
+: ${MODEL_NAME:=starcoderbase}
+
 # use podman by default
 # override by calling script with `DOCKER=docker ./run_starcoderbase.sh`
 : ${DOCKER:=podman}
@@ -22,7 +26,7 @@ $DOCKER run \
     -e NVIDIA_VISIBLE_DEVICES=$DEVICES \
     -e HF_HUB_ENABLE_HF_TRANSER=0 \
     ghcr.io/huggingface/text-generation-inference:0.8 \
-    --model-id /data/starcoderbase \
+    --model-id /data/$MODEL_NAME \
     --max-input-length 8192 \
     --max-total-tokens 16384 \
     --max-waiting-tokens 65536
