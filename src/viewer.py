5170768bc92939d29e218009dad3cac6e08dee49
Ming-Ho Yee <mh@mhyee.com>
2023-10-27 13:44:06 -0400

    fix bug in viewer

--- src/viewer.py
+++ src/viewer.py
@@ -62,20 +62,20 @@ class Viewer(cmd.Cmd):
             exit(2)
 
         # Pre-compute dataset summary
-        self.total_completions = np.sum(dataset["num_completions"])
+        self.total_completions = len([r for d in dataset for r in d["results"]])
         self.total_type_checks = np.sum(dataset["num_type_checks"])
-        self.pct_type_checks = self.total_type_checks / self.total_completions
+        self.pct_type_checks = 0 if self.total_completions == 0 else self.total_type_checks / self.total_completions
         self.avg_accuracy = np.mean(
-            [r["accuracy"] for d in dataset for r in d["results"]]
+            [r["accuracy"] for d in dataset for r in d["results"] if not r["error"]]
         )
         self.avg_levenshtein = np.mean(
-            [r["levenshtein"] for d in dataset for r in d["results"]]
+            [r["levenshtein"] for d in dataset for r in d["results"] if not r["error"]]
         )
         self.avg_type_errors = np.mean(
-            [r["type_errors"] for d in dataset for r in d["results"]]
+            [r["type_errors"] for d in dataset for r in d["results"] if not r["error"]]
         )
         self.avg_parse_errors = np.mean(
-            [r["parse_errors"] for d in dataset for r in d["results"]]
+            [r["parse_errors"] for d in dataset for r in d["results"] if not r["error"]]
         )
         self.pass_1 = np.mean(dataset["pass@1"])
 
