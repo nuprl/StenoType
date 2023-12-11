ea691ab16f55aec859a9f2b13d117da53d88ee01
Ming-Ho Yee <mh@mhyee.com>
2023-12-11 22:09:00 +0000

    [summarize] aggregate dataset-level stats

--- src/summarize.py
+++ src/summarize.py
@@ -9,6 +9,16 @@ from inference import Config
 from util import transform
 import util
 
+# TODO: A better approach would have been to flatten the datasets, one result
+# (note: a "result" is a single file of a completion of a problem of a dataset)
+# per row of a data frame, and then aggregate using pandas/R.
+
+# TODO: can maybe use some more helpers, but be careful about mixing
+# completion level, problem level, and dataset level
+
+# TODO: Should division by zero return 0 or None or nan? Need to handle these
+# cases, and also cases where data is missing
+
 
 def _pass_at_k(n: int, c: int, k: int) -> float:
     """
@@ -151,9 +161,15 @@ def _summarize_example(
 
     total_files = _sum(results, "num_files")
     total_errorfree_files = _sum(results, "num_errorfree_files")
-    summary["avg_errors"] = _mean_or_default(results, "num_errors", default=0.0)
-    summary["errors_per_file"] = _sum(results, "num_errors") / total_files
+    total_errors = _sum(results, "num_errors")
+    summary["tot_files"] = total_files
+    summary["tot_errorfree_files"] = total_errorfree_files
+    summary["tot_errors"] = total_errors
+    summary["errors_per_file"] = total_errors / total_files
     summary["pct_errorfree_files"] = total_errorfree_files / total_files
+
+    # TODO: errorfree isn't enough, we want types to have been added
+    # Probably want to distinguish project-level correct vs file-level correct
     summary["pass@1_errorfree"] = _pass_at_k(total_files, total_errorfree_files, 1)
 
     avg_fields = [
@@ -166,6 +182,7 @@ def _summarize_example(
         summary[f"avg_{f}"] = _mean_or_default(results, f, default=0.0)
 
     avg_fields_num = [
+        "num_errors",
         "num_annotation_sites",
         "num_annotations_added",
         "num_annotations_trivial",
@@ -178,6 +195,7 @@ def _summarize_example(
 
     total_annotations_trivial = _sum(results, "num_annotations_trivial")
     total_annotations_added = _sum(results, "num_annotations_added")
+    summary["num_annotation_sites"] = _sum(results, "num_annotation_sites")
     summary["num_annotations_trivial"] = total_annotations_trivial
     summary["num_annotations_added"] = total_annotations_added
     summary["pct_annotations_trivial"] = (
@@ -215,13 +233,13 @@ def _summarize_dataset(config: Config, args: argparse.Namespace) -> dict[str, An
             executor.submit(_summarize_example, idx, d) for idx, d in enumerate(dataset)
         ]
 
-        for f in tqdm(
+        for future in tqdm(
             futures.as_completed(fs),
             desc="Summarizing results",
             total=len(fs),
             miniters=1,
         ):
-            idx, result, summary = f.result()
+            idx, result, summary = future.result()
             results[idx], summaries[idx] = result, summary
 
     # Now write the results/summaries to the dataset
@@ -237,10 +255,94 @@ def _summarize_dataset(config: Config, args: argparse.Namespace) -> dict[str, An
     util.save_dataset(dataset, summary_output, args.workers)
     print()
 
-    # TODO: calculate dataset-level results (by aggregating problem stats)
-    # Return dictionary of dataset-level results
+    dataset_summary: dict[str, Any] = {
+        "model": config.filename,
+        "num_problems": len(dataset),
+    }
 
-    return {}
+    tot_completions = np.sum([s["num_completions"] for s in dataset["summaries"]])
+    dataset_summary["tot_completions"] = tot_completions
+
+    count_fields = ["num_correct", "num_failed", "num_parses", "num_type_checks"]
+    for f in count_fields:
+        total = np.sum([s[f] for s in dataset["summaries"]])
+        dataset_summary[f.replace("num", "tot")] = total
+        dataset_summary[f.replace("num", "pct")] = total / tot_completions
+    dataset_summary["pass@1_correct"] = _pass_at_k(
+        tot_completions, dataset_summary["tot_correct"], 1
+    )
+
+    count_error_fields = ["tot_files", "tot_errorfree_files", "tot_errors"]
+    for f in count_error_fields:
+        dataset_summary[f] = np.sum([s[f] for s in dataset["summaries"]])
+
+    total_files = np.sum([s["tot_files"] for s in dataset["summaries"]])
+    total_errorfree_files = np.sum(
+        [s["tot_errorfree_files"] for s in dataset["summaries"]]
+    )
+    total_errors = np.sum([s["tot_errors"] for s in dataset["summaries"]])
+    dataset_summary["tot_files"] = total_files
+    dataset_summary["tot_errorfree_files"] = total_errorfree_files
+    dataset_summary["tot_errors"] = total_errors
+    dataset_summary["errors_per_file"] = total_errors / total_files
+    dataset_summary["pct_errorfree_files"] = total_errorfree_files / total_files
+    dataset_summary["pass@1_errorfree"] = _pass_at_k(
+        total_files, total_errorfree_files, 1
+    )
+
+    avg_fields = [
+        "avg_annotation_sites",
+        "avg_annotations_added",
+        "avg_annotations_trivial",
+        "avg_definitions_added",
+        "avg_definitions_used",
+        "avg_types_undefined",
+        "avg_errors",
+        "avg_accuracy",
+        "avg_levenshtein",
+        "avg_untyped_levenshtein",
+        "avg_token_count",
+    ]
+    for f in avg_fields:
+        dataset_summary[f] = np.average(
+            [s[f] for s in dataset["summaries"]],
+            weights=[s["num_completions"] for s in dataset["summaries"]],
+        )
+
+    total_annotations_trivial = np.sum(
+        [s["num_annotations_trivial"] for s in dataset["summaries"]]
+    )
+    total_annotations_added = np.sum(
+        [s["num_annotations_added"] for s in dataset["summaries"]]
+    )
+    dataset_summary["pct_annotations_trivial"] = (
+        0
+        if total_annotations_added == 0
+        else (total_annotations_trivial / total_annotations_added)
+    )
+
+    tot_ann_trivial_errorfree = np.sum(
+        [s["num_annotations_trivial_errorfree_files"] for s in dataset["summaries"]]
+    )
+    tot_ann_added_errorfree = np.sum(
+        [s["num_annotations_added_errorfree_files"] for s in dataset["summaries"]]
+    )
+    dataset_summary["pct_annotations_trivial_errorfree_files"] = (
+        0
+        if tot_ann_added_errorfree == 0
+        else (tot_ann_trivial_errorfree / tot_ann_added_errorfree)
+    )
+
+    total_annotation_sites = np.sum(
+        [s["num_annotation_sites"] for s in dataset["summaries"]]
+    )
+    dataset_summary["pct_annotation_sites_filled"] = (
+        0
+        if total_annotation_sites == 0
+        else (total_annotations_added / total_annotation_sites)
+    )
+
+    return dataset_summary
 
 
 def summarize(configs: list[Config], args: argparse.Namespace):
@@ -249,4 +351,31 @@ def summarize(configs: list[Config], args: argparse.Namespace):
         summary = _summarize_dataset(config, args)
         summaries.append(summary)
 
-    # TODO: aggregate datasets and summarize everything, output to jsonl
+        print(f"===Stats for configuration {config.filename}===")
+        print(f"Number of problems: {summary['num_problems']}")
+        print(f"Total completions: {summary['tot_completions']}")
+        print(f"Failed: {summary['tot_failed']} ({summary['pct_failed']:.1%})")
+
+        print(f"Parses: {summary['tot_parses']} ({summary['pct_parses']:.1%})")
+        print(
+            f"Type checks: {summary['tot_type_checks']} ({summary['pct_type_checks']:.1%})"
+        )
+        print(f"Errors per problem: {summary['avg_errors']:.1f}")
+        print(f"Errors per file: {summary['errors_per_file']:.1f}")
+
+        print(f"Levenshtein: {summary['avg_levenshtein']:.1%}")
+        print(f"Untyped Levenshtein: {summary['avg_untyped_levenshtein']:.1%}")
+
+        print(f"Annotations added: {summary['avg_annotations_added']:.1f}")
+        print(f"Definitions added: {summary['avg_definitions_added']:.1f}")
+
+        print(f"Annotation sites filled: {summary['pct_annotation_sites_filled']:.1%}")
+        print(f"Trivial annotations: {summary['pct_annotations_trivial']:.1%}")
+        print(f"Definitions used: {summary['avg_definitions_used']:.1f}")
+
+        print(f"pass@1 (correct): {summary['pass@1_correct']:.1%}")
+        print(f"pass@1 (errorfree files): {summary['pass@1_errorfree']:.1%}")
+        print()
+
+    path = Path(args.results_directory, "summary.jsonl")
+    util.write_jsonl(path, summaries, util.NpEncoder)
